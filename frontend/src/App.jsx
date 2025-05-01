641be166451ca785dfc2745a58e80f09740e0fd0
Taylor <taybelle2020@gmail.com>
2025-05-01 14:20:07 -0400

    creating the about us tab, and the 3 pages available to signed in users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import { useContext, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import SignUpPage from './pages/SignUp';
-import LoginPage from './pages/Login';
-import SiteHeadingAndNav from './components/SiteHeadingAndNav';
-import NotFoundPage from './pages/NotFound';
-import UserContext from './contexts/current-user-context';
-import { checkForLoggedInUser } from './adapters/auth-adapter';
-import UsersPage from './pages/Users';
-import UserPage from './pages/User';
+import { useContext, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUpPage from "./pages/SignUp";
+import LoginPage from "./pages/Login";
+import SiteHeadingAndNav from "./components/SiteHeadingAndNav";
+import NotFoundPage from "./pages/NotFound";
+import UserContext from "./contexts/current-user-context";
+import { checkForLoggedInUser } from "./adapters/auth-adapter";
+import UsersPage from "./pages/Users";
+import UserPage from "./pages/User";
+import TeamPage from "./pages/team";
 
 export default function App() {
   const { setCurrentUser } = useContext(UserContext);
@@ -16,22 +17,25 @@ export default function App() {
     const loadCurrentUser = async () => {
       // we aren't concerned about an error happening here
       const [data] = await checkForLoggedInUser();
-      if (data) setCurrentUser(data)
-    }
+      if (data) setCurrentUser(data);
+    };
     loadCurrentUser();
   }, [setCurrentUser]);
 
-  return <>
-    <SiteHeadingAndNav />
-    <main>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/sign-up' element={<SignUpPage />} />
-        <Route path='/users' element={<UsersPage />} />
-        <Route path='/users/:id' element={<UserPage />} />
-        <Route path='*' element={<NotFoundPage />} />
-      </Routes>
-    </main>
-  </>;
+  return (
+    <>
+      <SiteHeadingAndNav />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/users" element={<UsersPage />} />
+          <Route path="/users/:id" element={<UserPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/team" element={<TeamPage />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
