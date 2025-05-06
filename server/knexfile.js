ecc1f95bd79b1f816149e3c4ecb18cd325fef818
Taylor <taybelle2020@gmail.com>
2025-05-06 12:01:47 -0400

    'main merged into taylor branch'

--- server/knexfile.js
+++ server/knexfile.js
@@ -16,11 +16,6 @@ module.exports = {
     client: "pg",
     connection: process.env.PG_CONNECTION_STRING || {
       host: process.env.PG_HOST || "127.0.0.1",
-      port: process.env.PG_PORT || 5432,
-      user: process.env.PG_USER || "postgres",
-      password: process.env.PG_PASS || "postgres",
-      database: process.env.PG_DB || "postgres",
-      host: process.env.PG_HOST || "127.0.0.1",
       port: process.env.PG_PORT || 5433,
       user: process.env.PG_USER || "postgres",
       password: process.env.PG_PASS || "postgres",
