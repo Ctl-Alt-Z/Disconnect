7b5bf7fe8f9d842d80e2b5c4c5d4b6ff79c9963c
Taylor <taybelle2020@gmail.com>
2025-05-06 11:49:11 -0400

    added email to sign up page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,19 +5,18 @@ import CurrentUserContext from "../contexts/current-user-context";
 
 export default function LoginPage() {
   const navigate = useNavigate();
-  const [errorText, setErrorText] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [errorText, setErrorText] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
-
   // users shouldn't be able to see the login page if they are already logged in.
-  // if the currentUser exists in the context, navigate the user to 
+  // if the currentUser exists in the context, navigate the user to
   // the /users/:id page for that user, using the currentUser.id value
   if (currentUser) return <Navigate to={`/users/${currentUser.id}`} />;
-
+  //
   const handleSubmit = async (event) => {
     event.preventDefault();
-    setErrorText('');
+    setErrorText("");
 
     const [user, error] = await logUserIn({ username, password });
     if (error) return setErrorText(error.message);
@@ -26,18 +25,34 @@ export default function LoginPage() {
     navigate(`/users/${user.id}`);
   };
 
-  return <>
-    <h1>Login</h1>
-    <form onSubmit={handleSubmit} aria-labelledby="login-heading">
-      <h2 id='login-heading'>Log back in!</h2>
-      <label htmlFor="username">Username</label>
-      <input type="text" autoComplete="username" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+  return (
+    <>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit} aria-labelledby="login-heading">
+        <h2 id="login-heading">Log back in!</h2>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          autoComplete="username"
+          id="username"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
 
-      <label htmlFor="password">Password</label>
-      <input type="password" autoComplete="current-password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          autoComplete="current-password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-      <button>Log in!</button>
-    </form>
-    {!!errorText && <p>{errorText}</p>}
-  </>;
+        <button>Log in!</button>
+      </form>
+      {!!errorText && <p>{errorText}</p>}
+    </>
+  );
 }
