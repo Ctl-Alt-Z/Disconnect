dcf24798c024e7dacd6124b63964dd268bb0151a
Taylor <taybelle2020@gmail.com>
2025-05-13 09:54:21 -0400

    having some errors, will commit work to try and get past it

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,16 @@
 export default function HomePage() {
   return (
     <>
-      <h1 id> Welcome to Disconnect</h1>
-      <p>Lets take the time to check-in and focus on ourselves</p>
-      <p>
-        we have acknowledged that you have been on your phone too much. You have
-        taken the first step in improving your focus in coming here! Log your
-        time, set some goals for your self and make a journal entry to inspire
-        others if you like!
-      </p>
+      <div id="homePage">
+        <h1 id="hpHeader"> Welcome to Disconnect</h1>
+        <p id="hpOne">Lets take the time to check-in and focus on ourselves</p>
+        <p id="hpMessage">
+          we have acknowledged that you have been on your phone too much. You
+          have taken the first step in improving your focus in coming here! Log
+          your time, set some goals for your self and make a journal entry to
+          inspire others if you like!
+        </p>
+      </div>
     </>
   );
 }
--- frontend/src/pages/Team.jsx
+++ frontend/src/pages/Team.jsx
@@ -1,16 +1,18 @@
 export default function TeamPage() {
   return (
     <>
-      <h1>About the Disconnect Team</h1>
+      <div id="teamPage">
+        <h1>About the Disconnect Team</h1>
 
-      <p>Image of the team</p>
-      <br></br>
-      <p>Luis, Zae, Avionte, Taylor </p>
-      <br></br>
-      <p>
-        message about what disconnect means to the team/ the intention of what
-        we want to do with this app.
-      </p>
+        <p>Image of the team</p>
+        <br></br>
+        <p>Luis, Zae, Avionte, Taylor </p>
+        <br></br>
+        <p id="tpMessage">
+          message about what disconnect means to the team/ the intention of what
+          we want to do with this app.
+        </p>
+      </div>
     </>
   );
 }
