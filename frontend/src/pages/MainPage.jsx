f0ab558c317b529fe88e3b6d0e523a3ef53f7ac8
Avionte Williams <aviontewilliams@gmail.com>
2025-05-14 14:15:11 -0400

    goal functionality

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -74,50 +74,11 @@ export default function MainPage() {
 
   return (
     <>
-      <GoalsForm />
       {showModal && <Preferences onClose={handleClose} />}
+      <GoalsForm />
       <div>
         <p> timer</p>
       </div>
-
-      <form>
-        <div>
-          <label>Set Daily Goal :</label>
-          <select value={textarea} onChange={handleChange}>
-            <option>1 hr</option>
-            <option>2 hrs</option>
-            <option>3 hrs</option>
-            <option>4 hrs</option>
-            <option>5 hrs</option>
-            <option>6 hrs</option>
-            <option>7 hrs</option>
-            <option>8 hrs</option>
-            <option>9 hrs</option>
-            <option>10 hrs</option>
-            <option>11 hrs</option>
-            <option>12 hrs</option>
-          </select>
-          <label>
-            {" "}
-            Goal expection:
-            <input type="text" />
-          </label>
-          {/* <textarea
-            value={textarea}
-            onChange={handleChange}
-            required
-          ></textarea> */}
-          {/* <div>
-            <label>GOAL 2:</label>
-            <textarea value={goalTwo} onChange={handleChangeTwo}></textarea>
-          </div> */}
-          {/* <div>
-            <label>GOAL 3:</label>
-            <textarea value={goalThree} onChange={handleChangeThree}></textarea>
-          </div> */}
-        </div>
-        <button>post</button>
-      </form>
       {/* end of goals/ start of journals */}
       <div>
         <br></br>
