fe03db7c56b277b3c525f11f86c63ade8e9d6540
Taylor <taybelle2020@gmail.com>
2025-05-15 11:36:35 -0400

    pushing what i have now to merge with main

--- frontend/src/components/statsTable.jsx
+++ frontend/src/components/statsTable.jsx
@@ -3,13 +3,20 @@ import { Bar } from "react-chartjs-2";
 import { Chart, registerables } from "chart.js";
 import { Chart as ChartJS } from "chart.js";
 
-const statsChart = () => {
+export default function StatsChart() {
   const [options, setOptions] = useState({
     title: {
       text: " User Screen time data",
     },
   });
-  //
 
-  return <></>;
-};
+  return (
+    <>
+      <BarChart>
+        xAxis={[{ data: ["group A", "group B", "group C"] }]}
+        series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+        height={300}
+      </BarChart>
+    </>
+  );
+}
