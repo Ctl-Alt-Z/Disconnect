b65e3c10eb89ed9c379882bd8a120f1662d40774
Taylor <taybelle2020@gmail.com>
2025-05-20 20:59:06 -0400

    added to goals, to make sure they are in sync with users and logs

--- server/db/seeds/04_goals.js
+++ server/db/seeds/04_goals.js
@@ -16,5 +16,12 @@ exports.seed = async function (knex) {
     { goal_num: 3, goal_string: "rowValue2", logs_id: 8 },
     { goal_num: 3, goal_string: "rowValue2", logs_id: 9 },
     { goal_num: 3, goal_string: "rowValue2", logs_id: 10 },
+    { goal_num: 5, goal_string: "newGoal1", logs_id: 11 },
+    { goal_num: 9, goal_string: "newGoal2", logs_id: 12 },
+    { goal_num: 2, goal_string: "newGoal3", logs_id: 13 },
+    { goal_num: 5, goal_string: "newGoal4", logs_id: 14 },
+    { goal_num: 5, goal_string: "newGoal5", logs_id: 15 },
+    { goal_num: 7, goal_string: "newGoal6", logs_id: 16 },
+    { goal_num: 4, goal_string: "newGoal7", logs_id: 17 },
   ]);
 };
